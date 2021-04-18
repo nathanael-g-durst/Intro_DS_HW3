aba804b309c12c675c5460d4601053c6fd1e60e4
nathanael-g-durst <nathanael.duerst@etu.unige.ch>
2021-04-18 02:09:38 +0200

    Small edits
    Removed install.packages and library
     - Replaced by check if installed
    
    Added par(pty="s") to specify the type of plot region
    
    Deleted the unused else part of the if statement
    
    Small edits comments

--- Exercice 1.R
+++ Exercice 1.R
@@ -1,24 +1,46 @@
-install.packages("plotrix")
-install.packages("scales")
-library(plotrix)
-library(scales)
+# Requirements
 
+## Check if installed, install it if not and load library
+installedPackages <- installed.packages()
+
+### Add here the packages needed ##############
+packagesNeeded <- c("plotrix", "scales")
+###############################################
+
+for (packageName in packagesNeeded) {
+  packageExists <- is.element(packageName, installedPackages)
+  if(packageExists != TRUE){
+    install.packages(packageName)
+    library(packageName, character.only = TRUE)
+    print(paste(packageName, "has been installed and the library is loaded !"))
+  } else {
+    library(packageName, character.only = TRUE)
+    print(paste(packageName, "is installed and the library is loaded !"))
+  }
+}
+
+## Clean environment
+rm(installedPackages, packageName, packagesNeeded, packageExists)
+
+# Function
 find_pi = function(B = 5000, seed = 10, make_plot = TRUE){
   # Control seed
   set.seed(seed)
   
-  # generate the x and y co-ordinates from uniform
-  # distribution taking values from -1 to 1
+  # Generate the x and y co-ordinates from uniform
+  # Distribution taking values from -1 to 1
   x<-runif(B, min=-1, max=1)
   y<-runif(B, min=-1, max=1)
   
   # Distance of the points from the center (0,0)
   z<-sqrt(x^2+y^2)
 
-  # radius=1
+  # Radius=1
   hat_pi = 4*sum((z<=1))/length(z)
   
+  # Plot
   if (make_plot == "TRUE"){
+  par(pty="s")
   plot(x,y,
        main="Estimate PI with Monte Carlo",
        xlim = c(-1,1),
@@ -30,11 +52,11 @@ find_pi = function(B = 5000, seed = 10, make_plot = TRUE){
        grid())
   rect( -1, -1, 1, 1) 
   draw.circle( 0, 0, 1 )
-  }else{
   }
+
   return(hat_pi)
+
 }
 
-
+# Execute function
 find_pi(make_plot = TRUE)
-
